847dda6de3e312eeaf44117e0d24f64c8f686af0
Joao Nascimento <nascimento.joao.dev@gmail.com>
2023-03-08 20:22:48 -0300

    Revert "add dist"
    This reverts commit 21cf52064374d09f0d5a18d0cc80817432eacb88.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/movies-wiki/",
+  base: "/vite-gh-pages",
   plugins: [react()],
 })
